56b92750f7733a02430705903182a3f7a10e9f01
tpartridge1 <tammy.partridge7@gmail.com>
2020-01-21 14:54:29 -0500

    edited toggle

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 function myFunction() {
-var x = document.getElementById('myDIV');
+var x = document.getElementById('myDIV'); 
 if (x.style.display === 'none') {
     x.style.display = 'block';
-} else {
-    x.style.display = 'none';
-   }
+} 
 }
-
+ 
 function myFunction2() {
 var x = document.getElementById('myDIV2');
 if (x.style.display === 'none') {
     x.style.display = 'block';
-} else {
-    x.style.display = 'none';
-   }
 }
+}
+
